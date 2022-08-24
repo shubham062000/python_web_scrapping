b7d8eecd635d27388cd0708f7de566ab3bd2ebf8
Shubham Torkade <92077877+shubham062000@users.noreply.github.com>
2022-08-24 17:53:09 +0530

    Add files via upload

---
+++ quotes_scrapping.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import pymongo
+
+def scrape_quotes():
+    more_links = True
+    page = 1
+    quotes = []
+    while(more_links):
+        markup = requests.get(f'http://quotes.toscrape.com/page/{page}').text
+        soup = BeautifulSoup(markup, 'html.parser')
+        for item in soup.select('.quote'):
+            quote = {}
+            quote['text'] = item.select_one('.text').get_text()
+            quote['author'] = item.select_one('.author').get_text()
+            tags = item.select_one('.tags')
+            quote['tags'] = [tag.get_text() for tag in tags.select('.tag')]
+            quotes.append(quote)
+
+        next_link = soup.select_one('.next > a')
+
+        # print which page was scraped
+        print(f'scraped page {page}')
+
+        # check if the next link element exists
+        if(next_link):
+            page += 1
+        else:
+            more_links = False
+    return quotes
+
+quotes = scrape_quotes()
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client.quotes_data.quotes
+try:
+    db.insert_many(quotes)
+    print(f'inserted {len(quotes)} articles into database successfully')
+except:
+    print('an error occurred quotes were not stored to database')
